d4caf69a514cf31663145c8be8f6bbe48217e57b
Peter Becich <peterbecich@gmail.com>
2022-01-15 14:29:48 -0800

    cross-compilation example

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,15 @@
 module Main where
 
+-- https://github.com/ghcjs/ghcjs-base#conversion-between-haskell-string-and-javascript-string
+import Data.JSString as S
+
+foreign import javascript unsafe
+  "require('console').log($1)" console_log :: S.JSString -> IO ()
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+  let message = "yo" :: String
+  console_log (S.pack message)
+
+-- main :: IO ()
+-- main = putStrLn "Hello, Haskell!"
--- readme.md
+++ readme.md
@@ -1,1 +1,5 @@
 Investigating https://github.com/input-output-hk/haskell.nix/issues/1244
+
+```
+nix-build -A projectCross.ghcjs.hsPkgs.ghcjs-example3.components.exes.ghcjs-example3
+```
