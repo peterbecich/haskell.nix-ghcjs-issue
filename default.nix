cf06badf60646a5a74c900cf46b95903d9e46585
Peter Becich <peter@simspace.com>
2022-03-04 14:16:23 -0800

    test private dependency

--- default.nix
+++ default.nix
@@ -4,10 +4,11 @@ let
   pkgs = import
     haskellNix.sources.nixpkgs-unstable
     haskellNix.nixpkgsArgs;
-in pkgs.pkgsCross.ghcjs.haskell-nix.project {
-  src = pkgs.pkgsCross.ghcjs.haskell-nix.haskellLib.cleanGit {
+in pkgs.haskell-nix.project {
+  src = pkgs.haskell-nix.haskellLib.cleanGit {
     name = "haskell-nix-project";
     src = ./.;
   };
   compiler-nix-name = "ghc8107";
+  projectFileName = "cabal.project";
 }
