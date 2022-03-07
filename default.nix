e2769520f47fdc6cc07e323923fe7fd60a9bab89
Peter Becich <peter@simspace.com>
2022-03-06 23:35:17 -0800

    test nixpkgs-21.11

--- default.nix
+++ default.nix
@@ -2,7 +2,7 @@ let
   sources = import ./nix/sources.nix {};
   haskellNix = import sources.haskellNix {};
   pkgs = import
-    haskellNix.sources.nixpkgs-unstable
+    haskellNix.sources.nixpkgs-2111
     haskellNix.nixpkgsArgs;
 in pkgs.haskell-nix.project {
   src = pkgs.haskell-nix.haskellLib.cleanGit {
