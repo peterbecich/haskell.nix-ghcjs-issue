5798682af9c1fe63dc6a4134be3df82ab7b64e32
Peter Becich <peterbecich@gmail.com>
2022-06-11 21:59:23 -0700

    import

--- app/Main.hs
+++ app/Main.hs
@@ -3,6 +3,8 @@ module Main where
 -- https://github.com/ghcjs/ghcjs-base#conversion-between-haskell-string-and-javascript-string
 import Data.JSString as S
 
+import GHCJS.Marshal (toJSVal_aeson)
+
 foreign import javascript unsafe
   "require('console').log($1)" console_log :: S.JSString -> IO ()
 
